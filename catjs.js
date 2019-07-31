1b941af4b7e0658067fab212f89e7ba7fc8320e6
Daniela <gomezq.daniela@gmail.com>
2019-07-31 16:00:21 -0400

    web dev

---
+++ catjs.js
@@ -0,0 +1,12 @@
+
+function switchImg(){
+  document.getElementById('winter').src='Winter2.JPG';
+}
+function readMore() {
+  var x = document.getElementById("hiddenP");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
